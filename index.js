a1d2c8e6ab4642f8b87cafd3e783104442c44295
scarter31191 <scarter31191@gmaile.com>
2021-03-23 19:47:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record) {
+    const that_one_year = record.find(won)
 
+    if (that_one_year) {
+        return that_one_year.year
+    } else {
+        return
+    }
+}
+
+function won(single_record) {
+    return single_record.result === "W"
+  }  
